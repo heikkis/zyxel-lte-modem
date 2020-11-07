29c928e8318858d3d528b11e2c628deac5c40eb4
Heikki Simperi <heikki.simperi@iki.fi>
2020-11-07 08:58:53 +0200

    Send error information by email and restart_policy to always to allow container service updates etc. to work.

--- server.js
+++ server.js
@@ -2,31 +2,36 @@ const {reboot} = require("./library");
 const {sendEmailNotification, login, manualModeConnect, manualModeDisconnect, pingGoogleDNS} = require("./library");
 
 (async () => {
-    let lastFailureDates = new Array()
-    while (true) {
-        console.log("START " + new Date().toISOString())
-        console.log("Latest failures: " + JSON.stringify(lastFailureDates))
+    try {
+        let lastFailureDates = new Array()
+        while (true) {
+            console.log("START " + new Date().toISOString())
+            console.log("Latest failures: " + JSON.stringify(lastFailureDates))
 
-        let pingres = await pingGoogleDNS();
-        if (!pingres.alive) {
-            console.log("ERROR: " + JSON.stringify(pingres))
-            if (lastFailureDates.length > 10) {
-                lastFailureDates = lastFailureDates.slice(lastFailureDates.length - 10)
+            let pingres = await pingGoogleDNS();
+            if (!pingres.alive) {
+                console.log("ERROR: " + JSON.stringify(pingres))
+                if (lastFailureDates.length > 10) {
+                    lastFailureDates = lastFailureDates.slice(lastFailureDates.length - 10)
+                }
+
+                lastFailureDates.push(new Date().toISOString())
+
+                await login();
+                await reboot();
+
+                await new Promise(resolve => setTimeout(resolve, 60000));
+                pingres = await pingGoogleDNS();
+                if (pingres.alive) {
+                    await sendEmailNotification(JSON.stringify(lastFailureDates));
+                }
             }
-
-            lastFailureDates.push(new Date().toISOString())
-
-            await login();
-            await reboot();
-
-            await new Promise(resolve => setTimeout(resolve, 60000));
-            pingres = await pingGoogleDNS();
-            if (pingres.alive) {
-                await sendEmailNotification(JSON.stringify(lastFailureDates));
-            }
+            console.log("END")
+            console.log("")
+            await new Promise(resolve => setTimeout(resolve, 15000));
         }
-        console.log("END")
-        console.log("")
-        await new Promise(resolve => setTimeout(resolve, 15000));
+    } catch(e) {
+        await sendEmailNotification(JSON.stringify(e));
+        throw e
     }
 })();
